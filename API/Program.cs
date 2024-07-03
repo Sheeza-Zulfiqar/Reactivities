27671af5d69bde42890d41c61f527f90aefd808f
Sheeza-Zulfiqar <sheezazulfiqar555@gmail.com>
2024-07-03 10:14:48 -0700

    Implemented walking skeleton of client side

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>{
+    opt.AddPolicy("CorsPolicy", policy=>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -23,7 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
+app.UseCors("CorsPolicy");
 app.MapControllers();
 using var scope=app.Services.CreateScope();
 var services=scope.ServiceProvider;
