10530e26294b84eea621db8e5b43c7ec51f61f99
Sheeza-Zulfiqar <sheezazulfiqar555@gmail.com>
2024-07-06 11:25:54 -0700

    confirgured axios, used generic types,  axios interceptors

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,15 +15,16 @@ interface Props{
     closeForm:()=>void;
     createOrEdit:(activity:Activity)=>void;
     deleteActivity:(id:string)=>void;
+    submitting:boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity,selectActivity,cancelSelectActivity
-    ,editMode,openForm,closeForm, createOrEdit,deleteActivity
+    ,editMode,openForm,closeForm, createOrEdit,deleteActivity, submitting
 }: Props){
     return(
         <Grid>
             <Grid.Column width='10'>
-                <AcitivityList activites={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
+                <AcitivityList submitting={submitting} activites={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -33,7 +34,7 @@ export default function ActivityDashboard({activities, selectedActivity,selectAc
                 openForm={openForm}
                 ></ActivityDetails>}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
             </Grid.Column>
         </Grid>
     )
